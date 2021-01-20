02df2a0fbc722832ed5207e0c94190a80b8ceba0
Mattias Kronberg <mattiaskronberg@me.com>
2021-01-20 11:25:16 +0100

    fix empty file

---
+++ src/infrastructure/GameServerDelegate.ts
@@ -0,0 +1,69 @@
+// DTO with appropriate information about joining players
+export type PlayerJoinedData = {
+    info: string
+}
+
+// Interface defining the events on which the game server consumer (delegate) might want to take action
+export interface GameServerDelegate {
+    OnPlayerJoined: (data: PlayerJoinedData) => void
+}
+
+// The game server interface
+export interface GameServer {
+    delegate: GameServerDelegate;
+
+    Join(): void;
+    Disconnect(): void;
+}
+
+// A fake implementation of game server
+export class MockGameServer implements GameServer {
+    
+    delegate: GameServerDelegate;
+
+    constructor(delegate: GameServerDelegate) {
+        // Do stuff
+        this.delegate = delegate;
+    }
+
+    Join(): void {
+        // Do stuff
+        this.playerJoined();
+    }
+    Disconnect(): void {
+        // Do stuff
+        // this.playerDisconnected();
+    }
+
+    playerJoined() {
+        var playerName = "Muggus Massus";
+        this.delegate.OnPlayerJoined({info: {playerName} + "joined"});
+    }
+}
+
+// An example of a consumer of game server (might be a React Component?)
+export class ServerConsumer implements GameServerDelegate {
+    
+    private _server: GameServer;
+
+    constructor() {
+        this._server = ServerFactory.GiveMeAGameServer(this);
+    }
+
+    // Maybe called from button or similar
+    RequestJoin() {
+        this._server.Join();
+    }
+
+    // Called when _server fires it (hopefully?)
+    OnPlayerJoined(data: PlayerJoinedData) {
+        // Update state
+    }
+}
+
+// An example of a server factory
+export class ServerFactory {
+    static GiveMeAGameServer(delegate: GameServerDelegate) : GameServer {
+        return new MockGameServer(delegate);
+    }
+}
