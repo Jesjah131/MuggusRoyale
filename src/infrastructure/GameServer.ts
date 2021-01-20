971bfe33176d0b37db99e4739e80c96897c2a74d
Mattias Kronberg <mattiaskronberg@me.com>
2021-01-20 15:55:55 +0100

    remove delegate solution

--- src/infrastructure/GameServer.ts
+++ src/infrastructure/GameServer.ts
@@ -3,22 +3,23 @@
 
     Not many stars/usages, so needs to be decided if safe to use?
 */
-import { SubEvent } from 'sub-events';
-import {MatchWaitingToStartData, MatchStartingData, MatchJoinedData} from './GameServerEventData';
-
-
+import {SubEvent} from 'sub-events';
+import {
+  MatchWaitingToStartData,
+  MatchStartingData,
+  MatchJoinedData,
+} from './GameServerEventData';
 
 // The interface for game server
 export interface GameServer {
+  OnMatchJoined: SubEvent<MatchJoinedData>;
+  OnMatchWaitingToStart: SubEvent<MatchWaitingToStartData>;
+  OnMatchStarting: SubEvent<MatchStartingData>;
 
-    OnMatchJoined: SubEvent<MatchJoinedData>
-    OnMatchWaitingToStart: SubEvent<MatchWaitingToStartData>
-    OnMatchStarting: SubEvent<MatchStartingData>
+  // Initialize connection to server
+  // ToDo: Do this in constructor?
+  Init(): void;
 
-    // Initialize connection to server
-    Init(): void
-
-    // Ask to join a server
-    RequestJoin(): void
+  // Ask to join a server
+  RequestJoin(): void;
 }
-
